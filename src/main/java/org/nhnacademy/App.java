c66b0fa493aa9933d95bf91b3296e2825df108a2
LeeJungBum <jungbum05114@gmail.com>
2023-09-13 14:30:02 +0900

    feat: 문제 2

--- src/main/java/org/nhnacademy/App.java
+++ src/main/java/org/nhnacademy/App.java
@@ -1,6 +1,7 @@
 package org.nhnacademy;
 
 import org.nhnacademy.domain.Dice;
+import org.nhnacademy.domain.Divisors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,8 +12,8 @@ public class App {
     private static Logger logger = LoggerFactory.getLogger(App.class);
 
     public static void main(String[] args) {
-        logger.info("{}",Q1());
-
+        logger.info("{}", Q1());
+        logger.info("1~10000중 가장 큰 값은? {}", Q2());
     }
 
     public static int Q1() {
@@ -23,9 +24,19 @@ public class App {
             pip1 = Dice.roll();
             pip2 = Dice.roll();
             count++;
-            logger.info("{}번째 시도 \n 1번 주사위 {} \n 2번 주사위 {}",count,pip1,pip2);
+            logger.info("{}번째 시도 \n 1번 주사위 {} \n 2번 주사위 {}", count, pip1, pip2);
         }
         return count;
+    }
+
+    public static int Q2() {
+        int max = 0;
+        for (int i = 1; i <= 10000; i++) {
+            int count = Divisors.count(i);
+            max = (max < count) ? i : max;
+        }
+
+        return max;
     }
 
     private static boolean isSnakeEye(int pip1, int pip2) {
---
+++ src/main/java/org/nhnacademy/domain/Divisors.java
@@ -0,0 +1,18 @@
+package org.nhnacademy.domain;
+
+public class Divisors {
+    private Divisors(){};
+    public static int count(int number){
+        int count =2;
+        int lastNum = number;
+        for (int i= count;i<lastNum;i++)
+        {
+            if(number%i==0)
+            {
+                lastNum = number/i;
+                count++;
+            }
+        }
+        return count;
+    }
+}
