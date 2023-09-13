7f74a47badba650e83c2d003b7124d4b8750b5b9
LeeJungBum <jungbum05114@gmail.com>
2023-09-13 14:03:44 +0900

    feat: 문제 1

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+Y
+)src/test/java/org/nhnacademy/AppTest.java,7/4/74e3f6c35137f99f8e6720c0d846737f50890515
+]
+-src/main/java/org/nhnacademy/domain/Dice.java,e/0/e04caa66d867e4dbaa0beaf287baa70d67919aa8
--- src/main/java/org/nhnacademy/App.java
+++ src/main/java/org/nhnacademy/App.java
@@ -1,13 +1,34 @@
 package org.nhnacademy;
 
+import org.nhnacademy.domain.Dice;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    private static Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        logger.info("{}",Q1());
+
+    }
+
+    public static int Q1() {
+        int pip1 = 0;
+        int pip2 = 0;
+        int count = 0;
+        while (!isSnakeEye(pip1, pip2)) {
+            pip1 = Dice.roll();
+            pip2 = Dice.roll();
+            count++;
+            logger.info("{}번째 시도 \n 1번 주사위 {} \n 2번 주사위 {}",count,pip1,pip2);
+        }
+        return count;
+    }
+
+    private static boolean isSnakeEye(int pip1, int pip2) {
+        return pip1 + pip2 == 2;
     }
 }
