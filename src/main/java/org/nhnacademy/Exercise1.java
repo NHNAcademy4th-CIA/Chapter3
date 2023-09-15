9c7d4a66be2ae6cbf0deabfc01fd104816d3c3df
minm063 <jangmin063@gmail.com>
2023-09-15 13:53:10 -0700

    refactor : do...while문으로 수정

--- src/main/java/org/nhnacademy/Exercise1.java
+++ src/main/java/org/nhnacademy/Exercise1.java
@@ -3,18 +3,15 @@ package org.nhnacademy;
 class Exercise1 {
     // 주사위 두 눈이 모두 1이 나올때까지의 굴린 횟수
 
-    private Exercise1() {
-    }
-
     public static int exercise1() {
-        int random1 = (int) (Math.random() * 6) + 1;
-        int random2 = (int) (Math.random() * 6) + 1;
+        int random1;
+        int random2;
         int count = 0;
-        while (!(random1 == 1 && random2 == 1)) {
+        do  {
             random1 = (int) (Math.random() * 6) + 1;
             random2 = (int) (Math.random() * 6) + 1;
             count++;
-        }
+        }while (!(random1 == 1 && random2 == 1));
         return count;
     }
 }
