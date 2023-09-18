94e21ab3fffd2690af6226e2c622312b8aaf2eb0
masiljangajji <hjk172262@gmail.com>
2023-09-18 16:14:18 +0900

    docs: README 추가

--- src/main/java/org/nhnacademy/lsj/Main.java
+++ src/main/java/org/nhnacademy/lsj/Main.java
@@ -6,14 +6,12 @@ import java.io.IOException;
  * Hello world!
  */
 public class Main {
-
-
     public static void main(String[] args) throws IOException {
 
 
         //Problem1.problem1();
         //Problem2.problem2();
-        Problem3.problem3();
+        //Problem3.problem3();
         //Problem4.problem4();
         //Problem5.problem5();
         //Problem6.problem6();
